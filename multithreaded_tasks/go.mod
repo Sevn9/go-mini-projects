1bbaf5650528a8c23552ab71ce7bce6bae28c5ff
Artur <72407422+Sevn9@users.noreply.github.com>
2025-10-02 23:57:48 +0300

    task 1 complete

---
+++ multithreaded_tasks/go.mod
@@ -0,0 +1,3 @@
+module github.com/Sevn9/go-mini-projects/multithreaded_tasks
+
+go 1.25.1
---
+++ multithreaded_tasks/task_1/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"sync/atomic"
+	"time"
+)
+
+func main() {
+
+	//способ 1 WaitGroup
+	var wg sync.WaitGroup
+
+	//способ 1.1
+	wg.Add(1)
+
+	func() {
+		defer wg.Done()
+		go helloFunc()
+	}()
+
+	wg.Wait()
+
+	//способ 1.2
+	wg.Go(func() {
+		go helloFunc()
+	})
+
+	wg.Wait()
+
+	//способ 2 канал завершения
+	doneChannel := make(chan struct{})
+	defer close(doneChannel)
+
+	go hello1Func(doneChannel)
+
+	<-doneChannel
+
+	//способ 3 atomic
+	var done int32
+
+	go func() {
+		helloFunc()
+		atomic.StoreInt32(&done, 1)
+	}()
+
+	for atomic.LoadInt32(&done) == 0 {
+		time.Sleep(10 * time.Millisecond)
+	}
+
+	fmt.Println("Main: end program")
+}
+
+func helloFunc() {
+	fmt.Println("Hello from goroutine!")
+}
+
+func hello1Func(doneChannel chan<- struct{}) {
+	fmt.Println("Hello from goroutine!")
+	doneChannel <- struct{}{}
+}
